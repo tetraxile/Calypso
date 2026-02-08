a3411c93274c7f9bcf2ca6939da4478149a37fe6
tetraxile <tetraxile@proton.me>
2026-02-08 19:39:49 +0100

    server: add networking

--- server/src/MainWindow.cpp
+++ server/src/MainWindow.cpp
@@ -16,14 +16,45 @@
 #include <QStatusBar>
 #include <QTableWidget>
 
+
+void MainWindow::newConnection() {
+    mClientSocket = mServer->nextPendingConnection();
+    
+    {
+        bool isV4 = false;
+        QHostAddress addrIPv6 = mClientSocket->peerAddress();
+        QHostAddress addrIPv4 = QHostAddress(addrIPv6.toIPv4Address(&isV4));
+        QString addrStr = isV4 ? addrIPv4.toString() : addrIPv6.toString();
+        quint16 port = mClientSocket->peerPort();
+        log("connection received from %s:%d", qPrintable(addrStr), port);
+    }
+
+    connect(mClientSocket, &QTcpSocket::disconnected, mClientSocket, &QTcpSocket::deleteLater);
+    connect(mClientSocket, &QTcpSocket::disconnected, this, &MainWindow::disconnected);
+    connect(mClientSocket, &QTcpSocket::readyRead, this, &MainWindow::readClient);
+}
+
+void MainWindow::disconnected() {
+    log("client disconnected");
+}
+
+void MainWindow::readClient() {
+    QTextStream in(mClientSocket);
+    log("[switch] %s", qPrintable(in.readAll()));
+}
+
 MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent) {
     setupUi();
+
+    mServer = new QTcpServer(this);
+    mServer->listen(QHostAddress::Any, 8171);
+    log("listening for connection on port %d...", 8171);
+    connect(mServer, &QTcpServer::newConnection, this, &MainWindow::newConnection);
 }
 
 MainWindow::~MainWindow() {
 	// delete mViewport;
 }
-
 
 void MainWindow::setupUi() {
     if (objectName().isEmpty())
@@ -34,146 +65,16 @@ void MainWindow::setupUi() {
     setCentralWidget(centralWidget);
 
     QVBoxLayout* vboxLayout = new QVBoxLayout(centralWidget);
-    QHBoxLayout* topRow    = new QHBoxLayout;
-    QHBoxLayout* bottomRow = new QHBoxLayout;
-    vboxLayout->addLayout(topRow);
-    vboxLayout->addLayout(bottomRow);
+    mTopRow    = new QHBoxLayout;
+    mBottomRow = new QHBoxLayout;
+    vboxLayout->addLayout(mTopRow);
+    vboxLayout->addLayout(mBottomRow);
 
-    {
-        QVBoxLayout* vboxLayout = new QVBoxLayout;
-        topRow->addLayout(vboxLayout);
-
-        QLabel* label = new QLabel(tr("Controls"));
-        label->setSizePolicy(QSizePolicy(QSizePolicy::Maximum, QSizePolicy::Maximum));
-        vboxLayout->addWidget(label);
-
-        QFrame* controlsWidget = new QFrame;
-        controlsWidget->setFrameShape(QFrame::StyledPanel);
-        vboxLayout->addWidget(controlsWidget);
-        QVBoxLayout* controlsLayout = new QVBoxLayout(controlsWidget);
-        // {
-        //     QFormLayout* row = new QFormLayout;
-        //     controlsLayout->addLayout(row);
-        //     QLineEdit* switchIPWidget = new QLineEdit;
-        //     switchIPWidget->setSizePolicy(QSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum));
-        //     row->addRow("Switch IP", switchIPWidget);
-        // }
-        {
-            QHBoxLayout* row = new QHBoxLayout;
-            controlsLayout->addLayout(row);
-
-            QComboBox* recentScripts = new QComboBox;
-            recentScripts->addItem("");
-            recentScripts->addItem("mreow");
-            recentScripts->addItem("mreow");
-            recentScripts->addItem("mreow");
-            row->addWidget(recentScripts);
-
-            QPushButton* openButton = new QPushButton(tr("Open"));
-            openButton->setSizePolicy(QSizePolicy(QSizePolicy::Maximum, QSizePolicy::Maximum));
-            row->addWidget(openButton);
-        }
-        {
-            QHBoxLayout* row = new QHBoxLayout;
-            controlsLayout->addLayout(row);
-
-            QPushButton* playButton = new QPushButton(tr("Run script"));
-            playButton->setDisabled(true);
-            row->addWidget(playButton);
-
-            QPushButton* stopButton = new QPushButton(tr("Stop script"));
-            stopButton->setDisabled(true);
-            row->addWidget(stopButton);
-        }
-        {
-            QHBoxLayout* row = new QHBoxLayout;
-            controlsLayout->addLayout(row);
-
-            QPushButton* pauseButton = new QPushButton(tr("Toggle game pause"));
-            pauseButton->setDisabled(true);
-            row->addWidget(pauseButton);
-
-            QPushButton* frameAdvanceButton = new QPushButton(tr("Frame advance"));
-            frameAdvanceButton->setDisabled(true);
-            row->addWidget(frameAdvanceButton);
-        }
-        {
-            QFormLayout* form = new QFormLayout;
-            controlsLayout->addLayout(form);
-
-            QLineEdit* scriptNameWidget = new QLineEdit;
-            scriptNameWidget->setSizePolicy(QSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum));
-            scriptNameWidget->setReadOnly(true);
-            form->addRow(tr("Script name"), scriptNameWidget);
-
-            QLineEdit* authorWidget = new QLineEdit;
-            authorWidget->setSizePolicy(QSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum));
-            authorWidget->setReadOnly(true);
-            authorWidget->setCursorPosition(0);
-            form->addRow(tr("Author"), authorWidget);
-
-            QLineEdit* commandCountWidget = new QLineEdit;
-            commandCountWidget->setSizePolicy(QSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum));
-            commandCountWidget->setReadOnly(true);
-            form->addRow(tr("Command count"), commandCountWidget);
-        }
-    }
-
-    {
-        QVBoxLayout* vboxLayout = new QVBoxLayout;
-        topRow->addLayout(vboxLayout);
-
-        QLabel* label = new QLabel(tr("Savestates"));
-        label->setSizePolicy(QSizePolicy(QSizePolicy::Maximum, QSizePolicy::Maximum));
-        vboxLayout->addWidget(label);
-
-        QFrame* savestatesWidget = new QFrame;
-        savestatesWidget->setFrameShape(QFrame::StyledPanel);
-        vboxLayout->addWidget(savestatesWidget);
-    }
-
-    {
-        QVBoxLayout* vboxLayout = new QVBoxLayout;
-        topRow->addLayout(vboxLayout);
-
-        QLabel* label = new QLabel(tr("Log"));
-        label->setSizePolicy(QSizePolicy(QSizePolicy::Maximum, QSizePolicy::Maximum));
-        vboxLayout->addWidget(label);
-
-        mLogWidget = new LogWidget;
-        mLogWidget->setFrameShape(QFrame::StyledPanel);
-        mLogWidget->setSizePolicy(QSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum));
-        vboxLayout->addWidget(mLogWidget);
-        
-        for (int i = 0; i < 100; i++)
-            mLogWidget->appendLine("meow");
-    }
-    
-    {
-        QVBoxLayout* vboxLayout = new QVBoxLayout;
-        bottomRow->addLayout(vboxLayout);
-
-        QLabel* label = new QLabel(tr("Input display"));
-        label->setSizePolicy(QSizePolicy(QSizePolicy::Maximum, QSizePolicy::Maximum));
-        vboxLayout->addWidget(label);
-
-        QFrame* inputDisplayWidget = new QFrame;
-        inputDisplayWidget->setFrameShape(QFrame::StyledPanel);
-        vboxLayout->addWidget(inputDisplayWidget);
-    }
-
-    {
-        QVBoxLayout* vboxLayout = new QVBoxLayout;
-        bottomRow->addLayout(vboxLayout);
-
-        QLabel* label = new QLabel("Game info");
-        label->setSizePolicy(QSizePolicy(QSizePolicy::Maximum, QSizePolicy::Maximum));
-        vboxLayout->addWidget(label);
-
-        QFrame* gameInfoWidget = new QFrame;
-        gameInfoWidget->setFrameShape(QFrame::StyledPanel);
-        vboxLayout->addWidget(gameInfoWidget);
-    }
+    setupControls();
+	setupSavestates();
+	setupLog();
+	setupInputDisplay();
+	setupGameInfo();
 
     mMenuBar = new QMenuBar(this);
     setMenuBar(mMenuBar);
@@ -196,4 +97,123 @@ void MainWindow::retranslateUi() {
     // togglePauseButton->setText(QCoreApplication::translate("MainWindow", "Toggle game pause", nullptr));
     // frameAdvanceButton->setText(QCoreApplication::translate("MainWindow", "Frame advance", nullptr));
     menuMeow->setTitle(QCoreApplication::translate("MainWindow", "Meow", nullptr));
+}
+
+void MainWindow::addSection(QFrame* section, const QString& name, QHBoxLayout* row) {
+	QVBoxLayout* vboxLayout = new QVBoxLayout;
+	row->addLayout(vboxLayout);
+
+	QLabel* label = new QLabel(name);
+	label->setSizePolicy(QSizePolicy(QSizePolicy::Maximum, QSizePolicy::Maximum));
+	vboxLayout->addWidget(label);
+
+	section->setFrameShape(QFrame::StyledPanel);
+	vboxLayout->addWidget(section);
+}
+
+void MainWindow::setupControls() {
+	QFrame* controlsWidget = new QFrame;
+	addSection(controlsWidget, tr("Controls"), mTopRow);
+
+	QVBoxLayout* controlsLayout = new QVBoxLayout(controlsWidget);
+	// {
+	//     QFormLayout* row = new QFormLayout;
+	//     controlsLayout->addLayout(row);
+	//     QLineEdit* switchIPWidget = new QLineEdit;
+	//     switchIPWidget->setSizePolicy(QSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum));
+	//     row->addRow("Switch IP", switchIPWidget);
+	// }
+	{
+		QHBoxLayout* row = new QHBoxLayout;
+		controlsLayout->addLayout(row);
+
+		QComboBox* recentScripts = new QComboBox;
+		recentScripts->addItem("");
+		recentScripts->addItem("mreow");
+		recentScripts->addItem("mreow");
+		recentScripts->addItem("mreow");
+		row->addWidget(recentScripts);
+
+		QPushButton* openButton = new QPushButton(tr("Open"));
+		openButton->setSizePolicy(QSizePolicy(QSizePolicy::Maximum, QSizePolicy::Maximum));
+		row->addWidget(openButton);
+	}
+	{
+		QHBoxLayout* row = new QHBoxLayout;
+		controlsLayout->addLayout(row);
+
+		QPushButton* playButton = new QPushButton(tr("Run script"));
+		playButton->setDisabled(true);
+		row->addWidget(playButton);
+
+		QPushButton* stopButton = new QPushButton(tr("Stop script"));
+		stopButton->setDisabled(true);
+		row->addWidget(stopButton);
+	}
+	{
+		QHBoxLayout* row = new QHBoxLayout;
+		controlsLayout->addLayout(row);
+
+		QPushButton* pauseButton = new QPushButton(tr("Toggle game pause"));
+		pauseButton->setDisabled(true);
+		row->addWidget(pauseButton);
+
+		QPushButton* frameAdvanceButton = new QPushButton(tr("Frame advance"));
+		frameAdvanceButton->setDisabled(true);
+		row->addWidget(frameAdvanceButton);
+	}
+	{
+		QFormLayout* form = new QFormLayout;
+		controlsLayout->addLayout(form);
+
+		QLineEdit* scriptNameWidget = new QLineEdit;
+		scriptNameWidget->setSizePolicy(QSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum));
+		scriptNameWidget->setReadOnly(true);
+		form->addRow(tr("Script name"), scriptNameWidget);
+
+		QLineEdit* authorWidget = new QLineEdit;
+		authorWidget->setSizePolicy(QSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum));
+		authorWidget->setReadOnly(true);
+		authorWidget->setCursorPosition(0);
+		form->addRow(tr("Author"), authorWidget);
+
+		QLineEdit* commandCountWidget = new QLineEdit;
+		commandCountWidget->setSizePolicy(QSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum));
+		commandCountWidget->setReadOnly(true);
+		form->addRow(tr("Command count"), commandCountWidget);
+	}
+}
+
+void MainWindow::setupSavestates() {
+	QFrame* savestatesWidget = new QFrame;
+	addSection(savestatesWidget, tr("Savestates"), mTopRow);
+}
+
+void MainWindow::setupLog() {
+	mLogWidget = new LogWidget;
+	addSection(mLogWidget, tr("Log"), mTopRow);
+	mLogWidget->setSizePolicy(QSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum));
+}
+
+void MainWindow::setupInputDisplay() {
+	QFrame* inputDisplayWidget = new QFrame;
+	addSection(inputDisplayWidget, tr("Input display"), mBottomRow);
+}
+
+void MainWindow::setupGameInfo() {
+	QFrame* gameInfoWidget = new QFrame;
+	addSection(gameInfoWidget, tr("Game info"), mBottomRow);
+
+	QFormLayout* form = new QFormLayout(gameInfoWidget);
+	gameInfoWidget->setLayout(form);
+
+	QLineEdit* stageName = new QLineEdit;
+	stageName->setSizePolicy(QSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum));
+	stageName->setReadOnly(true);
+	form->addRow(tr("Stage name"), stageName);
+
+	QLineEdit* playerPos = new QLineEdit;
+	playerPos->setSizePolicy(QSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum));
+	playerPos->setReadOnly(true);
+	form->addRow(tr("Player position"), playerPos);
 }
--- server/src/MainWindow.h
+++ server/src/MainWindow.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <QHBoxLayout>
 #include <QMainWindow>
+#include <QTcpSocket>
+#include <QTcpServer>
 
 #include "LogWidget.h"
 
@@ -14,12 +17,40 @@ public:
     void setupUi();
     void retranslateUi();
 
-    void log(const QString& text) { mLogWidget->appendLine(text); }
+#ifdef __GNUC__
+    [[gnu::format(printf, 2, 3)]]
+#endif
+    void log(const char* fmt, ...) {
+        va_list args;
+        va_start(args, fmt);
+
+        QString out = QString::vasprintf(fmt, args);
+
+        mLogWidget->appendLine(out);
+
+        va_end(args);
+    }
+
+private slots:
+    void newConnection();
+    void disconnected();
+    void readClient();
 
 private:
-    LogWidget* mLogWidget = nullptr;
+    void setupControls();
+	void setupSavestates();
+	void setupLog();
+	void setupInputDisplay();
+	void setupGameInfo();
+    void addSection(QFrame* section, const QString& name, QHBoxLayout* row);
 
+    LogWidget* mLogWidget;
+    QHBoxLayout* mBottomRow;
+    QHBoxLayout* mTopRow;
     QMenuBar *mMenuBar;
     QMenu *menuMeow;
     QStatusBar *mStatusBar;
+
+    QTcpServer* mServer = nullptr;
+    QTcpSocket* mClientSocket = nullptr;
 };
